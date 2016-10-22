b0ee8298fb8d3c9f786ee6be36cdd778623e0884
tibuchuba <acsaxsax95@gmail.com>
2016-10-21 23:32:43 -0500

    Add JunitTest

---
+++ src/JUnitTest/rtpTesting.java
@@ -0,0 +1,14 @@
+package JUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class rtpTesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
